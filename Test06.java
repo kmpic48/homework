082dbaae2b6f66e4be21bd38c2327c425df7c3e5
kmpic48 <130130731+kmpic48@users.noreply.github.com>
2023-04-07 15:31:17 +0900

    Add files via upload

---
+++ Test06.java
@@ -0,0 +1,27 @@
+package day0404;
+
+public class Test06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 0;
+		 
+		System.out.println(++a);
+		System.out.println(++a);
+		System.out.println(++a);
+		System.out.println(++a);
+		System.out.println(++a);
+		System.out.println(++a);
+		
+		int i = 0;
+		
+		for (int i = 0; i < 9 ; i++ ) {
+			System.out.println(i);	
+		}
+			
+	
+
+	}
+
+}
